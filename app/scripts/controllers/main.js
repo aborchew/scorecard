34e8ed73c0da68507e03507e158c221ca9b0e6c6
aborche <ari.borchew@searshc.com>
2014-01-09 18:30:28 -0600

    Touching up GUI

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,4 +4,7 @@ angular.module('scorecardApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
     $scope.game = new Game();
     console.log($scope.game);
+    $scope.runnerClick = function(r){
+      $scope.game.runners.advance(r,1);
+    }
   }]);
